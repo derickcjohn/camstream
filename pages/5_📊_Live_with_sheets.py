608fde615330d2b55b9a5882870c79bc97384281
derickcjohn <96041141+derickcjohn@users.noreply.github.com>
2024-03-02 21:00:14 +0530

    Update 5_📊_Live_with_sheets.py

--- pages/5_📊_Live_with_sheets.py
+++ pages/5_📊_Live_with_sheets.py
@@ -4,4 +4,5 @@ from streamlit_gsheets import GSheetsConnection
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
+df = conn.read(spreadsheet=url, usecols=[0, 1,2,3])
+st.dataframe(df)
