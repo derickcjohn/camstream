f77047fc33305343ab9f43182263e8922e826d11
derickcjohn <96041141+derickcjohn@users.noreply.github.com>
2024-03-02 21:26:26 +0530

    Update 5_📊_Live_with_sheets.py

--- pages/5_📊_Live_with_sheets.py
+++ pages/5_📊_Live_with_sheets.py
@@ -1,5 +1,34 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+
+
+image = Image.open('icon.png')
+
+st.set_page_config(page_title="Cam stream Data - Live", 
+                   page_icon=image, 
+                   initial_sidebar_state="expanded",
+                  layout="wide")
+
+# hide_streamlit_style = """
+#             <style>
+#             header {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.sidebar.success("Select a page from above")
+
+st.markdown("""
+<style>
+[data-testid="stDateInput"] [data-baseweb="input"]:before {
+    content: url(https://i.imgur.com/pIZPHar.jpg) !important;
+    padding-top: 8px !important;
+    padding-left: 7px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.header("Exploring Camera Stream Data")
+# st.text("This page allows you to delve into your Camstream data collected on various dates.")
 url = "https://docs.google.com/spreadsheets/d/11o-ZoNmn4-FdCHd0gJuZMrUC-3mYFgo2EheHKIJfhvE/edit?usp=sharing"
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
