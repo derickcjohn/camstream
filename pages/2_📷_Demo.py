2604f1a91c8442bbf83b252e60aa4e93d01f3f0d
derickcjohn <96041141+derickcjohn@users.noreply.github.com>
2024-03-19 19:25:27 +0530

    Update 2_📷_Demo.py

--- pages/2_📷_Demo.py
+++ pages/2_📷_Demo.py
@@ -22,15 +22,16 @@ st.set_page_config(page_title="Cam stream Data - Demo",
 #             </style>
 #             """
 # st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-st.sidebar.page_link(
-  "1_📊_Live.py",  
-  label="Live",
-  icon="📊")
-st.sidebar.page_link(
-  "https://camstream.streamlit.app/Demo",  
-  label="Demo",
-  icon="📷",
-  disabled=True)
+# st.sidebar.page_link(
+#   "1_📊_Live.py",  
+#   label="Live",
+#   icon="📊")
+# st.sidebar.page_link(
+#   "https://camstream.streamlit.app/Demo",  
+#   label="Demo",
+#   icon="📷",
+#   disabled=False)
+st.sidebar.success("Select a page from above")
 
 st.header("Webstream for Live Object Detection")
 st.info("This is a Demo of simple Object Detection.", icon="ℹ️")
