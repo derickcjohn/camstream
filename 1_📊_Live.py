d7051196cbd5f38713c0384ca31d95d34a584b66
derickcjohn <96041141+derickcjohn@users.noreply.github.com>
2024-03-19 13:16:20 +0530

    Update 1_📊_Live.py

--- 1_📊_Live.py
+++ 1_📊_Live.py
@@ -74,13 +74,13 @@ st.divider()
 
 data['time-stamp'] = pd.to_datetime(data['time-stamp'], format='%d/%m/%Y %H:%M:%S')
 set_date = list(set(data['time-stamp']))
-st.write('Hello',set_date)
+st.write(set_date)
 min_date = data['time-stamp'].min().date()
 max_date = data['time-stamp'].max().date()
 selected_date = st.date_input("Select Date", value=None, min_value=min_date, 
                               max_value=max_date, format="DD/MM/YYYY")
 
-if selected_date is None:
+if selected_date is None or not in set_date:
    st.info("Select a Date", icon="ℹ")
    st.stop()
 
