94fd2efc5c545ce179c58e7c27f9a99c6495485a
derickcjohn <96041141+derickcjohn@users.noreply.github.com>
2024-03-20 20:36:37 +0530

    Update 3_exp_live.py

--- pages/3_exp_live.py
+++ pages/3_exp_live.py
@@ -126,15 +126,15 @@ if col1.button("Previous") and page > 1:
     end_index = min(start_index + rows_per_page, len(data))
     page_data = display_paginated_dataframe(data, start_index, end_index)
     data_display_placeholder.empty()  # Clear previous DataFrame
-    data_display_placeholder = display_paginated_dataframe(data, start_index, end_index)
+    data_display_placeholder.display_paginated_dataframe(data, start_index, end_index)
 
 if col2.button("Next") and page < total_pages:
     page += 1
     start_index = (page - 1) * rows_per_page
     end_index = min(start_index + rows_per_page, len(data))
-    page_data = display_paginated_dataframe(data, start_index, end_index)
-    data_display_placeholder.empty()  # Clear previous DataFrame
-    data_display_placeholder = display_paginated_dataframe(data, start_index, end_index)
+    page_data = data_display_placeholder.display_paginated_dataframe(data, start_index, end_index)
+    # data_display_placeholder.empty()  # Clear previous DataFrame
+    # data_display_placeholder.display_paginated_dataframe(data, start_index, end_index)
   
 # with st.expander("Data Preview"):
   # st.info("New data is constantly added. Click 'R' to refresh and view it.", icon="ℹ")
