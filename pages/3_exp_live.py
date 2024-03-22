5fcca5e1aca66a5a41228d2b82f27934434a4993
derickcjohn <96041141+derickcjohn@users.noreply.github.com>
2024-03-22 11:47:52 +0530

    Update 3_exp_live.py

--- pages/3_exp_live.py
+++ pages/3_exp_live.py
@@ -147,22 +147,22 @@ st.info("Data preview (paginated, showing {} rows per page).".format(page_size),
 pagination = st.container()
 
 bottom_menu = st.columns((4, 1, 1))
-    with bottom_menu[2]:
-        batch_size = st.selectbox("Page Size", options=[25, 50, 100])
-    with bottom_menu[1]:
-        total_pages = (
-            int(len(data) / batch_size) if int(len(data) / batch_size) > 0 else 1
-        )
-        current_page = st.number_input(
-            "Page", min_value=1, max_value=total_pages, step=1
-        )
-    with bottom_menu[0]:
-        st.markdown(f"Page **{current_page}** of **{total_pages}** ")
+with bottom_menu[2]:
+    batch_size = st.selectbox("Page Size", options=[25, 50, 100])
+with bottom_menu[1]:
+    total_pages = (
+        int(len(data) / batch_size) if int(len(data) / batch_size) > 0 else 1
+    )
+    current_page = st.number_input(
+        "Page", min_value=1, max_value=total_pages, step=1
+    )
+with bottom_menu[0]:
+    st.markdown(f"Page **{current_page}** of **{total_pages}** ")
 
 
 
-    pages = split_frame(data, batch_size)
-    pagination.dataframe(data=pages[current_page - 1], use_container_width=True)
+pages = split_frame(data, batch_size)
+pagination.dataframe(data=pages[current_page - 1], use_container_width=True)
                   
 # with st.expander("Data Preview"):
 #   st.info("New data is constantly added. Click 'R' to refresh and view it.", icon="ℹ")
