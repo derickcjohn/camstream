d0f586f6bc0fc78e23802d00a668d0fdccf02d2f
derickcjohn <96041141+derickcjohn@users.noreply.github.com>
2024-03-26 14:27:26 +0530

    Update 1_📊_Live.py

--- 1_📊_Live.py
+++ 1_📊_Live.py
@@ -90,7 +90,8 @@ set_date = set(data['time-stamp'].dt.date)
 
 min_date = data['time-stamp'].min().date()
 max_date = data['time-stamp'].max().date()
-selected_date = st.date_input("Date", value=None, min_value=min_date, 
+left_column, right_column = st.columns(2)
+selected_date = left_column.date_input("Date", value=None, min_value=min_date, 
                               max_value=max_date, format="DD/MM/YYYY")
 
 if selected_date is None:
